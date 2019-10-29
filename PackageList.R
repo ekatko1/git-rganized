3e59b77dd2f86b3df3b4c527e7c2aad24fa2bec4
ekatko1 <egor.katkov@gmail.com>
2019-10-29 15:11:03 -0400

    Package list to send out to workshop attendees to speed things up.

---
+++ PackageList.R
@@ -0,0 +1,7 @@
+## Package requirements for Git-rganized Workshop
+
+# Knitr for knitting documents from code
+install.packages(c('knitr', 'emmeans'), dependencies = TRUE) 
+
+# Optionally, install tidyverse to help you with the analysis:
+install.packages("tidyverse")
