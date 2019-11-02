fba1024b5664bd2466b21c9e298e7851823da96f
ekatko1 <egor.katkov@gmail.com>
2019-11-02 15:17:55 -0400

    Clean dir of useless files

--- PackageList.R
+++
@@ -1,7 +0,0 @@
-## Package requirements for Git-rganized Workshop
-
-# Knitr for knitting documents from code
-install.packages(c('knitr', 'emmeans'), dependencies = TRUE) 
-
-# Optionally, install tidyverse to help you with the analysis:
-install.packages("tidyverse")
